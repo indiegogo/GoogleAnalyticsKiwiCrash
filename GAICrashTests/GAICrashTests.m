e9dd6261408c79f1abb4ba8de61aeac147eae54f
Kevin Xie <kevin.xie@indiegogo.com>
2015-08-19 14:31:07 -0700

    Working Example of the Crash

--- GAICrashTests/GAICrashTests.m
+++ GAICrashTests/GAICrashTests.m
@@ -5,36 +5,19 @@
 //  Created by Kevin Xie on 8/19/15.
 //  Copyright (c) 2015 Kevin Xie. All rights reserved.
 //
+#import <Kiwi/Kiwi.h>
 
-#import <UIKit/UIKit.h>
-#import <XCTest/XCTest.h>
+SPEC_BEGIN(test)
+describe(@"In our first Kiwi test", ^{
+    context(@"a sample string", ^{
+        NSString *greeting = @"Hi";//Hello, World!";
+        it(@"should exist.", ^{
+            [greeting shouldBeNil];
+        });
+        it(@"should be 'Hello, World!'.", ^{
+            [[greeting should] equal:@"Hello, World!"];
+        });
+    });
+});
 
-@interface GAICrashTests : XCTestCase
-
-@end
-
-@implementation GAICrashTests
-
-- (void)setUp {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
-
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
-- (void)testExample {
-    // This is an example of a functional test case.
-    XCTAssert(YES, @"Pass");
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
-}
-
-@end
+SPEC_END
---
+++ Podfile
@@ -0,0 +1,11 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '8.0'
+
+pod 'GoogleAnalytics-iOS-SDK'
+pod 'Kiwi'
+
+target :specs do
+#    pod 'GoogleAnalytics-iOS-SDK'
+#    pod 'Kiwi'
+    link_with 'GAICrashTests'
+end
